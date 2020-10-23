72c6591f9b0d27a29f43c5349246f81363589432
Michael Peter <michaelpeteronline@googlemail.com>
2020-10-23 15:14:21 +0200

    Inital

---
+++ BlazorLinkTestApp/wwwroot/scripts/TestWebComponent.js
@@ -0,0 +1,12 @@
+﻿
+customElements.define('webcomp-link', class extends HTMLElement {
+    constructor() {
+        super(); // always call super() first in the constructor.
+
+        // Attach a shadow root to <fancy-tabs>.
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.innerHTML = `
+          <a href="Counter">ShadowDOM Link</a>
+    `;
+    }
+});
